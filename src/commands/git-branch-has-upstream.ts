381a2d7494b4bee6537a0c506186d0e67046a2a9
Wei Sun <sunwei415@126.com>
2023-04-14 01:42:59 +0800

    refactor(git-branch-has-upstream.ts): remove unused reject parameter in Promise fix(git-branch-has-upstream.ts): resolve with false if git command fails

--- src/commands/git-branch-has-upstream.ts
+++ src/commands/git-branch-has-upstream.ts
@@ -1,7 +1,7 @@
 import { exec, execSync } from "child_process";
 
 export async function hasUpstream(): Promise<boolean> {
-  return new Promise<boolean>((resolve, reject) => {
+  return new Promise<boolean>((resolve) => {
     exec('git rev-parse --abbrev-ref --symbolic-full-name @{u}', (err, stdout, stderr) => {
       if (err) {
         resolve(false);
