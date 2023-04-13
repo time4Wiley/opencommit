3f5247cc6ece082974aa4096f962cd4bc7105f6e
Wei Sun <sunwei415@126.com>
2023-04-14 01:42:26 +0800

    fix(git-branch-has-upstream.ts): resolve promise with false instead of rejecting when git rev-parse command fails

--- src/commands/git-branch-has-upstream.ts
+++ src/commands/git-branch-has-upstream.ts
@@ -4,9 +4,9 @@ export async function hasUpstream(): Promise<boolean> {
   return new Promise<boolean>((resolve, reject) => {
     exec('git rev-parse --abbrev-ref --symbolic-full-name @{u}', (err, stdout, stderr) => {
       if (err) {
-        reject(err);
+        resolve(false);
       } else if (stderr) {
-        reject(new Error(stderr.trim()));
+        resolve(false);
       } else {
         const upstreamBranch: string = stdout.trim();
         const currentBranch: string = execSync('git rev-parse --abbrev-ref HEAD').toString().trim();
