9a6b4f2e7de986128d142ecc4e758bb6143c8b30
Wei Sun <sunwei415@126.com>
2023-05-08 11:12:52 +0800

    opencommit add option after_commit shell commands to execute after the commit is done feat(commit-with-message-and-push.ts): add support for optional parameters --inserting_message, --should_push, and --after_commit

--- src/commands/commit-with-message-and-push.ts
+++ src/commands/commit-with-message-and-push.ts
@@ -5,26 +5,26 @@ import { hasUpstream } from "./git-branch-has-upstream";
 
 export function done() {
   outro(`${chalk.green("🌕 ✔✔✔")} Done!`);
+}
 
+function getValueFromArgs(paramName: string, defaultValue: any = null, extraArgs: string[]): any {
+  const index = extraArgs.indexOf(paramName);
+  let value = defaultValue;
+  if (index !== -1) {
+    value = extraArgs[index + 1];
+    extraArgs.splice(index, 2);
+  }
+  return value;
 }
 
 export async function commitWithMessageAndPush(commitMessage: string, extraArgs: string[]) {
   const isCommitConfirmedByUser = true;
 
   if (isCommitConfirmedByUser && !isCancel(isCommitConfirmedByUser)) {
-    const insertingMessageIndex = extraArgs.indexOf("--inserting_message");
-    let messageToCommit = commitMessage;
-    if (insertingMessageIndex !== -1) {
-      messageToCommit = `${extraArgs[insertingMessageIndex + 1]}\n${commitMessage}`;
-      extraArgs.splice(insertingMessageIndex, 2);
-    }
+    const messageToCommit = getValueFromArgs("--inserting_message", commitMessage, extraArgs);
+    const shouldPush = getValueFromArgs("--should_push", false, extraArgs);
 
-    const shouldPushIndex = extraArgs.indexOf("--should_push");
-    let shouldPush = false;
-    if (shouldPushIndex !== -1) {
-      shouldPush = extraArgs[shouldPushIndex + 1] === "true";
-      extraArgs.splice(shouldPushIndex, 2);
-    }
+    const afterCommit = getValueFromArgs("--after_commit", null, extraArgs);
 
     const { stdout } = await execa("git", [
       "commit",
@@ -34,13 +34,19 @@ export async function commitWithMessageAndPush(commitMessage: string, extraArgs:
     ]);
 
     outro(`${chalk.green("✔")} successfully committed`);
-
     outro(stdout);
 
     if (shouldPush && await hasUpstream()) {
       const { stdout } = await execa("git", ["push", ...extraArgs]);
       if (stdout) outro(stdout);
       outro(`${chalk.green("✔")} successfully pushed`);
+    }
+
+    // afterCommit is a string containing shell commands to run after the commit
+    if (afterCommit) {
+      const { stdout } = await execa(afterCommit, [...extraArgs]);
+      if (stdout) outro(stdout);
+      outro(`${chalk.green("✔")} successfully ran after_commit`);
     }
 
     done();
